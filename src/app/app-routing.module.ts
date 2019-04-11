2a74c4d2abc93ba83d290309932c3a45c3c4858f
BLOT Martial <martial.blot@outlook.fr>
2019-04-11 13:58:30 +0200

    un peu de style pour le form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BookingFormsComponent } from './booking-forms/booking-forms.component';
+
 
 const routes: Routes = [];
+
+// {path: '/bookings/:id', component: BookingFormsComponent}
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+
+
+  }
