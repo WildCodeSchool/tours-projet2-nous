56d9e34adce0cacfe8c7322f6079351928f298fb
Cyprien De Roef <c.deroef@outlook.fr>
2019-04-12 11:16:31 +0200

    categorie done + establishment inscription init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // tslint:disable-next-line: max-line-length
 import { EstablishmentInscriptionComponent } from './establishment-inscription/establishment-inscription.component';
+import { CategorieComponent } from './categorie/categorie.component';
 
 const routes: Routes = [
   { path: 'establishments/:id', component: EstablishmentInscriptionComponent },
+  { path: '', component: CategorieComponent },
 ];
 
 @NgModule({
